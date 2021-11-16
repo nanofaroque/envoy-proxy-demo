13839d5322d496631dcff3a02ef629cf077f19e7
nanofaroque <faroque.ae@gmail.com>
2021-11-16 00:07:02 -0600

    path update

--- greenlight/app/app.py
+++ greenlight/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/green')
 def hello():
     return '''<!DOCTYPE html>
 <html>
--- redlight/app/app.py
+++ redlight/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/red')
 def hello():
     return '''<!DOCTYPE html>
 <html>
